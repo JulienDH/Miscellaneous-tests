dda4e3a62e5dc4caababed739e8e84a7a4cf2f80
JulienDH <j.dhainaut@live.com>
2016-02-27 12:00:00 +0100

    functions tests

--- temp.py
+++ temp.py
@@ -1,9 +1,12 @@
+#coding=utf-8
 
-def parallelepipede(longueur, largeur, hauteur):
-    longueur, largeur, hauteur = int(longueur), int(largeur), int(hauteur)
-    print "le volume du parallelepipede est de : " + str(longueur*largeur*hauteur)
+def parallelepipede(length, width, height):
+    #Calculate the parallelepipoid volume
+    length, width, height = int(length), int(width), int(height)
+    print "le volume du parallelepipede est de : " + str(length*width*height)
 
 def time_convertor(number):
+    #seconds conversion
     seconds = int(number)
     minutes = seconds/60
     hours = minutes/24
@@ -11,3 +14,27 @@ def time_convertor(number):
     months = days/30
     years = months/12
     print "Number of seconds = " + str(seconds) + "\nNumber of minutes = " + str(minutes) + "\nNumber of days = " + str(days) + "\nNumber of months = " + str(months) + "\nNumber of years = " + str(years)
+
+def multiple(number):
+    #Create a list of number multiple of the args
+    #After each number modulo 3, a * is inserted
+    liste = []
+    for i in range(1,20):
+        liste.append(i*number)
+    for element in liste:
+        if element != "*" and (element%3 == 0):
+            liste.insert(liste.index(element)+1,"*")
+    return liste
+
+def multi(number):
+    #the shorter way of the multiple function
+    for i in range(1,20):
+        number = i*number
+        print number,
+        if number%3==0:
+            print "*",
+
+def picto(number):
+    #draw a half fir tree
+    for i in range(1,number):
+        print i * "*"
