30163a958b521246b100a0e71c02281b043e60d9
JulienDH <j.dhainaut@live.com>
2016-02-29 00:52:33 +0100

    function tests

--- temp.py
+++ temp.py
@@ -38,3 +38,56 @@ def picto(number):
     #draw a half fir tree
     for i in range(1,number):
         print i * "*"
+
+def rise(max):
+    liste = [1]
+    for i in xrange(0,max):
+        liste.append(liste[i]*2)
+    return liste[max]
+
+def letter_count(word, letter):
+    i = 0
+    for element in word:
+        if element == letter:
+            i+=1
+    print 'The letter: {} found {} time' .format(letter, i)
+
+def insert_star(word):
+    new_word = ""
+    for letter in word:
+        new_word = new_word + letter + "*"
+    return new_word
+
+def palindrome(word):
+    new_word = ""
+    for letter in word:
+        new_word = letter + new_word
+    if word ==  new_word:
+        print "Oui c'est un palindrome"
+    else:
+        print "Non ce n'est pas un palindrome"
+
+def top_nbr():
+    liste = [132, 5, 12, 8, 3, 75, 2, 85]
+    biggest = liste[0]
+    length = len(liste)
+    i = 0
+    while i <= length-1:
+        if liste[i] > biggest:
+            biggest = liste[i]
+        i+=1
+    print 'The largest number is {}' .format(biggest)
+
+def pair_impair():
+    liste = [132, 5, 12, 8, 3, 75, 2, 85]
+    length = len(liste)
+    even = []
+    odd = []
+    i = 0
+    while i <= length-1:
+        if liste[i]%2==0:
+            even.append(liste[i])
+        else:
+            odd.append(liste[i])
+        i+=1
+    print "Even elements:\n" + str(even) + "\nOdd elements:\n" + str(odd)
